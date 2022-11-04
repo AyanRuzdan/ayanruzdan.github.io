6480116fe5f79f8374c07941f7427d081fc8bfc6
Ayan Ruzdan <ayanruzdan@gmail.com>
2022-11-04 22:04:27 +0530

    4th Nov

---
+++ array_input.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; //Size of array
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n]; // Make an array of size n
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    return 0;
+}
