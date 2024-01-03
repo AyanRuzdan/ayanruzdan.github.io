14847f1881a7353f2f69ef8bb132794ca85af231
Ayan Ruzdan <ayanruzdan@gmail.com>
2024-01-03 19:40:37 +0530

    added timer

---
+++ new1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define ll long long int
+#define lb cout << endl;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}
